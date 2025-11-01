72fb6332ba5ad50680cf99c803e2e09dcfcede33
mamafati <abufati876@gmail.com>
2025-11-01 11:52:02 +0000

    feat: added propertyadd section and modified navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -238,12 +238,16 @@ const Navbar = () => {
             className= {`${pathname === '/properties/add' ? 'bg-black' : ''}text-white block rounded-md px-3 py-2 text-base font-medium`} 
             >Add Property</Link>
             )}
+            {!session && providers &&   Object.values(providers).map((provider,  index) => (
           <button
+          onClick={()=> signIn(provider.id)}
+          key={index}
             className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
           >
-            <i className="fa-brands fa-google mr-2"></i>
+             <FaGoogle className='mr-2' />
             <span>Login or Register</span>
           </button>
+          ),)}
         </div>
       </div> )}
     </nav>
